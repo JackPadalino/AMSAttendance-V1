790fabdba91d317654ae824cc02c7c555d3f2618
JackPadalino <pada0867@gmail.com>
2022-12-21 23:22:28 -0500

    updated file

--- server/api/message.js
+++ server/api/message.js
@@ -2,8 +2,26 @@ const express = require("express");
 const router = express.Router();
 const sequelize = require("sequelize");
 
-router.get('/',(req, res, next) => {
-    res.send('This route hit!');
+const accountSID = 'AC7eab81725083f55a1fa723f10becc390';
+const authToken = 'f2a00fb35148b94ecc2721827c0194c9';
+const client = require('twilio')(accountSID,authToken);
+
+router.get('/send',(req, res, next) => {
+    try {
+        const {
+            message,
+            sender,
+            receiver
+        } = req.body;
+        client.messages.create({
+            body:message,
+            from:sender,
+            to:receiver
+        });
+        res.sendStatus(200);
+    }catch(error){
+        next(error);
+    };
 });
 
 module.exports = router;
