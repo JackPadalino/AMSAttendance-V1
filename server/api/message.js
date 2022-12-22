a8f1cb47e642d6ef3181fe2689d93f6d531841e8
JackPadalino <pada0867@gmail.com>
2022-12-22 01:26:02 -0500

    able to receive and respond to incoming text message

--- server/api/message.js
+++ server/api/message.js
@@ -4,8 +4,11 @@ const sequelize = require("sequelize");
 
 const accountSID = 'AC7eab81725083f55a1fa723f10becc390';
 const authToken = 'f2a00fb35148b94ecc2721827c0194c9';
+const apiKey = 'SK0b6b01a1233fa3bf8d522ecbf5b2ca1a';
 const client = require('twilio')(accountSID,authToken);
+const { MessagingResponse } = require('twilio').twiml;
 
+// GET localhost:3000/api/message/send
 router.get('/send',(req, res, next) => {
     try {
         const {
@@ -19,6 +22,17 @@ router.get('/send',(req, res, next) => {
             to:receiver
         });
         res.sendStatus(200);
+    }catch(error){
+        next(error);
+    };
+});
+
+// POST localhost:3000/api/message/receive
+router.post('/receive',(req, res, next) => {
+    try {
+        const twiml = new MessagingResponse();
+        twiml.message('BeEp BoOp BeEp!');
+        res.type('text/xml').send(twiml.toString());
     }catch(error){
         next(error);
     };
