76c86951c3b382f55f865655abd046170e302eec
JackPadalino <pada0867@gmail.com>
2022-12-23 09:30:18 -0500

    updated model

--- server/db/Message.js
+++ server/db/Message.js
@@ -2,6 +2,11 @@ const db = require("./db");
 const Sequelize = require("sequelize");
 
 const Message = db.define("message", {
+  // id: {
+  //     type: UUID,
+  //     primaryKey: true,
+  //     defaultValue: UUIDV4
+  // },
   content: {
     type: Sequelize.STRING
   }
