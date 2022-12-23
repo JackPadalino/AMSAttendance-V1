78651eca295330e538483b0581a621538a39faf1
JackPadalino <pada0867@gmail.com>
2022-12-22 21:32:17 -0500

    updated route to respond to incoming texts!

--- server/api/message.js
+++ server/api/message.js
@@ -4,22 +4,23 @@ const sequelize = require("sequelize");
 
 const accountSID = process.env.AMSATT_TWILIO_ACCOUNT_SID;
 const authToken = process.env.AMSATT_TWILIO_AUTHTOKEN;
+const twilioNumber = process.env.AMSATT_TWILIO_NUMBER;
 const client = require('twilio')(accountSID,authToken);
 const { MessagingResponse } = require('twilio').twiml;
 
 // GET localhost:3000/api/message/send
 router.get('/send',(req, res, next) => {
     try {
-        const {
-            message,
-            sender,
-            receiver
-        } = req.body;
-        client.messages.create({
-            body:message,
-            from:sender,
-            to:receiver
-        });
+        // const {
+        //     message,
+        //     receiver
+        // } = req.body;
+        // client.messages.create({
+        //     body:message,
+        //     from:twilioNumber,
+        //     to:receiver
+        // });
+        console.log(twilioNumber);
         res.sendStatus(200);
     }catch(error){
         next(error);
@@ -29,9 +30,11 @@ router.get('/send',(req, res, next) => {
 // POST localhost:3000/api/message/receive
 router.post('/receive',(req, res, next) => {
     try {
+        const message = req.body.Body;
         const twiml = new MessagingResponse();
-        twiml.message('BeEp BoOp BeEp!');
-        res.type('text/xml').send(twiml.toString());
+        message==='1' ? twiml.message('Sick day requested. Please wait for a confirmation message from Admin.') : twiml.message('Please enter a 1 to proceed.')
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
     }catch(error){
         next(error);
     };
