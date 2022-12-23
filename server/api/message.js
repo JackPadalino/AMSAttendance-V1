68115fd811e171e29df51146d5ad1b184edf4bdc
JackPadalino <pada0867@gmail.com>
2022-12-23 00:58:05 -0500

    updated route to respond to text message values

--- server/api/message.js
+++ server/api/message.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const sequelize = require("sequelize");
-
-const accountSID = process.env.AMSATT_TWILIO_ACCOUNT_SID;
-const authToken = process.env.AMSATT_TWILIO_AUTHTOKEN;
-const twilioNumber = process.env.AMSATT_TWILIO_NUMBER;
-const client = require('twilio')(accountSID,authToken);
 const { MessagingResponse } = require('twilio').twiml;
+const { Message } = require("../db");
+const {accountSID,authToken,twilioNumber} = require("../../bin/env");
+const client = require('twilio')(accountSID,authToken);
 
 // GET localhost:3000/api/message/send
 router.get('/send',(req, res, next) => {
     try {
-        // const {
-        //     message,
-        //     receiver
-        // } = req.body;
-        // client.messages.create({
-        //     body:message,
-        //     from:twilioNumber,
-        //     to:receiver
-        // });
-        console.log(twilioNumber);
+        const {
+            message,
+            receiver
+        } = req.body;
+        client.messages.create({
+            body:message,
+            from:twilioNumber,
+            to:receiver
+        });
         res.sendStatus(200);
     }catch(error){
         next(error);
@@ -28,11 +25,18 @@ router.get('/send',(req, res, next) => {
 });
 
 // POST localhost:3000/api/message/receive
-router.post('/receive',(req, res, next) => {
+router.post('/receive', async(req, res, next) => {
     try {
         const message = req.body.Body;
         const twiml = new MessagingResponse();
-        message==='1' ? twiml.message('Sick day requested. Please wait for a confirmation message from Admin.') : twiml.message('Please enter a 1 to proceed.')
+        if(message==='1'){
+            twiml.message('Sick day requested. Please wait for a confirmation message from Admin.');
+            await Message.create({
+                content:'Some one called out!'
+            });
+        }else{
+            twiml.message('Please enter a 1 to proceed.');
+        };
         res.writeHead(200, {'Content-Type': 'text/xml'});
         res.end(twiml.toString());
     }catch(error){
