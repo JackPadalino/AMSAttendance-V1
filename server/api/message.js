72f0bca747ceac1383b7bbb6b77b6d545dfa8baa
JackPadalino <pada0867@gmail.com>
2022-12-24 15:04:58 -0500

    creating new day from phone number

--- server/api/message.js
+++ server/api/message.js
@@ -31,44 +31,50 @@ router.post('/receive', async(req, res, next) => {
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const dateStr = `${year}-${month}-${day}`;
-    let todaysDate = '';
-    const foundDate = await Day.findAll({
-        where:{
-            date:dateStr
-        }
+    await Day.create({
+        date:dateStr
     });
-    if(!foundDate){
-        date = await Day.create({
-            date:date
-        })
-    };
-    try {
-        const sender = req.body.From; 
-        const teacher = await Teacher.findAll({
-            where:{
-                phoneNumber:sender
-            }
-        });
-        await Absence.create({
-            teacher:teacher,
-            day:todaysDate
-        });
-        //const message = req.body.Body;
-        //const twiml = new MessagingResponse();
-        // if(message==='1'){
-        //     twiml.message('Sick day requested. Please wait for a confirmation message from Admin.');
-        //     await Message.create({
-        //         content:'Some one called out!'
-        //     });
-        // }else{
-        //     twiml.message('Please enter a 1 to proceed.');
-        // };
-        // res.writeHead(200, {'Content-Type': 'text/xml'});
-        // res.end(twiml.toString());
-        res.send(200);
-    }catch(error){
-        next(error);
-    };
+    // let todaysDate={};
+    // const foundDate = await Day.findAll({
+    //     where:{
+    //         date:dateStr
+    //     }
+    // });
+    // if(foundDate){
+    //     todaysDate=foundDate;
+    // }else{
+    //     const newDate = await Day.create({
+    //         date:dateStr
+    //     });
+    //     todaysDate = newDate;
+    // };
+    // try {
+    //     console.log(todaysDate);
+    //     const teacher = await Teacher.findAll({
+    //         where:{
+    //             phoneNumber:req.body.From
+    //         }
+    //     });
+    //     await Absence.create({
+    //         teacher:teacher,
+    //         day:todaysDate
+    //     });
+    //     const message = req.body.Body;
+    //     const twiml = new MessagingResponse();
+    //     if(message==='1'){
+    //         twiml.message('Sick day requested. Please wait for a confirmation message from Admin.');
+    //         await Message.create({
+    //             content:'Some one called out!'
+    //         });
+    //     }else{
+    //         twiml.message('Please enter a 1 to proceed.');
+    //     };
+    //     res.writeHead(200, {'Content-Type': 'text/xml'});
+    //     res.end(twiml.toString());
+    //     res.send(200);
+    // }catch(error){
+    //     next(error);
+    // };
 });
 
 module.exports = router;
