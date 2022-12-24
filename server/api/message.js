4524693d9595b41ae59f00ba33ae257887c37555
JackPadalino <pada0867@gmail.com>
2022-12-24 16:26:40 -0500

    updated function to create new days and absences from message

--- server/api/message.js
+++ server/api/message.js
@@ -25,56 +25,49 @@ router.get('/send',(req, res, next) => {
 });
 
 // POST localhost:3000/api/message/receive
+// NEED TO ADD THE ABILITY TO CHECK IF SOME ONE HAS ALREADY CALLED OUT ON THIS FUNCTION
 router.post('/receive', async(req, res, next) => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const dateStr = `${year}-${month}-${day}`;
-    await Day.create({
-        date:dateStr
-    });
-    // let todaysDate={};
-    // const foundDate = await Day.findAll({
-    //     where:{
-    //         date:dateStr
-    //     }
-    // });
-    // if(foundDate){
-    //     todaysDate=foundDate;
-    // }else{
-    //     const newDate = await Day.create({
-    //         date:dateStr
-    //     });
-    //     todaysDate = newDate;
-    // };
-    // try {
-    //     console.log(todaysDate);
-    //     const teacher = await Teacher.findAll({
-    //         where:{
-    //             phoneNumber:req.body.From
-    //         }
-    //     });
-    //     await Absence.create({
-    //         teacher:teacher,
-    //         day:todaysDate
-    //     });
-    //     const message = req.body.Body;
-    //     const twiml = new MessagingResponse();
-    //     if(message==='1'){
-    //         twiml.message('Sick day requested. Please wait for a confirmation message from Admin.');
-    //         await Message.create({
-    //             content:'Some one called out!'
-    //         });
-    //     }else{
-    //         twiml.message('Please enter a 1 to proceed.');
-    //     };
-    //     res.writeHead(200, {'Content-Type': 'text/xml'});
-    //     res.end(twiml.toString());
-    //     res.send(200);
-    // }catch(error){
-    //     next(error);
-    // };
+    try {
+        const message = req.body.Body;
+        const twiml = new MessagingResponse();
+        if(message==='1'){
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+            const dateStr = `${year}-${month}-${day}`;
+            let todaysDate={};
+            const foundDate = await Day.findOne({
+                where:{
+                    date:dateStr
+                }
+            });
+            if(foundDate){
+                todaysDate=foundDate;
+            }else{
+                const newDate = await Day.create({
+                    date:dateStr
+                });
+                todaysDate = newDate;
+            };
+            const teacher = await Teacher.findOne({
+                where:{
+                    phoneNumber:req.body.From
+                }
+            });
+            await Absence.create({
+                teacherId:teacher.id,
+                dayId:todaysDate.id
+            });
+            twiml.message('Sick day requested. Please wait for a confirmation message from Admin.');
+        }else{
+            twiml.message('Please enter a 1 to proceed.');
+        };
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
+    }catch(error){
+        next(error);
+    };
 });
 
 module.exports = router;
