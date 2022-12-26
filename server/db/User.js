082eab17b0aca991eb148a760e25e397590fd669
JackPadalino <pada0867@gmail.com>
2022-12-26 13:34:56 -0500

    instal bcrypt module

--- server/db/User.js
+++ server/db/User.js
@@ -1,9 +1,9 @@
 const db = require("./db");
 const Sequelize = require("sequelize");
-// const { STRING, UUID, UUIDV4 } = db.Sequelize;
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-// const JWT = process.env.JWT;
+const { STRING, UUID, UUIDV4 } = db.Sequelize;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT = process.env.JWT;
 
 const User = db.define("user", {
   // id: {
@@ -64,5 +64,44 @@ const User = db.define("user", {
     defaultValue:'teacher'
   }
 });
+
+User.addHook("beforeSave", async (user) => {
+  if (user.changed("password")) {
+    user.password = await bcrypt.hash(user.password, 5);
+  }
+});
+
+User.findByToken = async function (token) {
+  try {
+    const { id } = jwt.verify(token, process.env.JWT);
+    const user = await this.findByPk(id);
+    if (user) {
+      return user;
+    }
+    throw "user not found";
+  } catch (ex) {
+    const error = new Error("bad credentials");
+    error.status = 401;
+    throw error;
+  }
+};
+
+User.prototype.generateToken = function () {
+  return jwt.sign({ id: this.id }, JWT);
+};
+
+User.authenticate = async function ({ username, password }) {
+  const user = await this.findOne({
+    where: {
+      username,
+    },
+  });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    return jwt.sign({ id:user.id,username:user.username,role:user.role }, JWT);
+  }
+  const error = new Error("bad credentials");
+  error.status = 401;
+  throw error;
+};
 
 module.exports = User;
