c0bbcb1e4cf1a6d0fd1a0328231bd0111e4c4dee
JackPadalino <pada0867@gmail.com>
2022-12-27 12:54:01 -0500

    deleted TeacherCoverage component

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,3 @@ export { default as Navbar } from './Navbar';
 export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as Admin } from './Admin';
-export { default as TeacherCoverages } from './TeacherCoverages';
