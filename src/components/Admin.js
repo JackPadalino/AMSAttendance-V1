ef67e1e0d1352274f80b81fd233730a6010a8e5c
JackPadalino <pada0867@gmail.com>
2022-12-28 18:38:25 -0500

    updated store and redux to persist selected date

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,39 +1,39 @@
 import axios from 'axios';
 import React, { useState,useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { NotFoundPage } from "./";
+import { setDate } from "../store/dateSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 const Admin = () => {
-    const [date,setDate] = useState('');
+    //const [date,setDate] = useState('');
+    const dispatch = useDispatch();
+    const { date } = useSelector((state) => state.date);
     const [userAbsences,setUserAbsences] = useState([]);
     const [token, setToken] = useState(window.localStorage.getItem("token"));
 
     const handleDateChange =  async (event) => {
         const date = event.target.value;
-        setDate(date);
+        dispatch(setDate(date));
         const absences = await axios.get(`/api/attendance/absences/${date}`);
-        // Old code - Using a for loop to make a separate API call for each absence object
+        // new code - using Promise.all to make a single API call to fetch all the user details in one go
+        const userPromises = absences.data.map(absence => axios.get(`/api/users/${absence.user.id}`));
+        const userResponses = await Promise.all(userPromises);
+        const userAbsences = userResponses.map(response => response.data);
+        setUserAbsences(userAbsences);
+        // old code - using a for loop to make a separate API call for each absence object
         // const absArr = [];
         // for(let absence of absences.data){
         //     const user = await axios.get(`/api/users/${absence.user.id}`);
         //     absArr.push(user.data);
         // };
         // setUserAbsences(absArr);
-
-        // New code - Use Promise.all to make a single API call to fetch all the user details in one go
-        const userPromises = absences.data.map(absence => axios.get(`/api/users/${absence.user.id}`));
-        const userResponses = await Promise.all(userPromises);
-        const userAbsences = userResponses.map(response => response.data);
-        setUserAbsences(userAbsences);
-        
     };
 
     if(!token) return <NotFoundPage/>
     return (
         <div>
-            <input type="date" id="date" onChange={handleDateChange}></input>
+            <input type="date" id="date" value={date} onChange={handleDateChange}></input>
             <h1>Attendance {date}</h1>
             <div>
                 {userAbsences.length>0 && userAbsences.map((user) => {
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import classReducer from "./classSlice";
+import dateReducer from "./dateSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
-    class:classReducer
+    class:classReducer,
+    date:dateReducer
   },
 });
 
