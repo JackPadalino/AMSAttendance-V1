3fee30a75eb8ff969ff32c44312b40d84dd2b570
JackPadalino <pada0867@gmail.com>
2022-12-28 18:29:24 -0500

    added new API routes to find class info and available teachers

--- server/api/classes.js
+++ server/api/classes.js
@@ -15,4 +15,32 @@ router.get('/',async(req, res, next) => {
     };
 });
 
+// GET localhost:3000/api/classes/:classId
+router.get('/:classId',async(req, res, next) => {
+    try {
+        const classInfo = await Class.findByPk(req.params.classId,{
+            include:[User]
+        });
+        res.send(classInfo);
+    }catch(error){
+        next(error);
+    };
+});
+
+// GET localhost:3000/api/classes/coverages/:period
+router.get('/coverages/:period',async(req, res, next) => {
+    try {
+        const freePeriod = await Class.findOne({
+            where:{
+                period:req.params.period,
+                isFreePeriod:true
+            },
+            include:[User]
+        });
+        res.send(freePeriod);
+    }catch(error){
+        next(error);
+    };
+});
+
 module.exports = router;
