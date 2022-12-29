15ba53303142b4e6f8855e4348dce57fc58c6edd
JackPadalino <pada0867@gmail.com>
2022-12-29 01:38:13 -0500

    removed dateSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import classReducer from "./classSlice";
-import dateReducer from "./dateSlice";
 import adminReducer from './adminSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     class:classReducer,
-    admin:adminReducer,
-    date:dateReducer
+    admin:adminReducer
   },
 });
 
